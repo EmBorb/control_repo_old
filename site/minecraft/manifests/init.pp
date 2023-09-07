fd1f5dd18df01ffc668e384763e5b6a2dca100bd
Emily Barker <86636490+EmBorb@users.noreply.github.com>
2023-09-07 14:44:00 +0800

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -6,7 +6,7 @@ class minecraft {
     ensure => file,
     source => 'https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar'
   }
-  file {'./jdk-17_linux-x64_bin.rpm':
+  file {'/opt/minecraft/jdk-17_linux-x64_bin.rpm':
   source => 'puppet:///modules/java/jdk-8u161-linux-x64.rpm',
   }
    package { 'java':
