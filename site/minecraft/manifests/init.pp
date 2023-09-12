bbdec520a1ea4f3705fb8cb535056619de76f24d
Emily Barker <86636490+EmBorb@users.noreply.github.com>
2023-09-12 08:53:53 +0800

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,6 +1,7 @@
-class minecraft {
-  $url = 'https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar'
+class minecraft (
+  $url = 'https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar',
   $install_dir = '/opt/minecraft'
+){
   file {'/opt/minecraft':
     ensure => directory,
   }
